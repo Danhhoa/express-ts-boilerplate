3f92f1c788564e0e0d00aa4c004d86e82cc3716a
Hoa Thanh Danh (VN.OP-ENG) <ThanhDanh.Hoa@ttigroup.com.vn>
2025-01-24 13:45:29 +0700

    chore: remove unuse libs

--- src/@types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@/modules/user/user.entity';
-import { Knex } from 'knex/types';
-
-declare module 'knex/types/tables' {
-    interface Tables {
-        user: User;
-        book: any;
-    }
-}
--- src/shared/utilities/date-time.utility.ts
+++ src/shared/utilities/date-time.utility.ts
@@ -1,7 +1,7 @@
-import moment from 'moment';
+import dayjs from 'dayjs';
 
 export default class DateTimeUtility {
-    static getCurrentTimeStamp() {
-        return moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
-    }
+  static getCurrentTimeStamp() {
+    return dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+  }
 }
